22fd755fd957111c3cb6b6d0515675c972ccaad5
Keuvain <118433891+Sarulpabi1@users.noreply.github.com>
2024-01-09 12:03:03 +0100

    Terrain + target ennemy, rotation turret

--- TowerDefense_Quest/Assets/Scripts/towerLogic.cs
+++ TowerDefense_Quest/Assets/Scripts/towerLogic.cs
@@ -1,18 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class turret : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Transform target;
+    public float range = 10f;
+
+    public string enemyTag = "Enemy";
+    public Transform turretPartRotation;
+
+    public float turnSpeed = 7f;
+
+    private void Start()
     {
-        
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+    private void UpdateTarget()
+    {
+        GameObject[] ennemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in ennemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (target == null)
+        {
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion look = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(turretPartRotation.rotation, look, Time.deltaTime * turnSpeed).eulerAngles;
+        turretPartRotation.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
